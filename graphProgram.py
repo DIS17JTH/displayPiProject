9418163e18c4dabbae3501611c2a87bd52cbb14c
malu1798 <malu1798@student.ju.se>
2019-01-02 13:40:13 +0100

    formatted

--- graphProgram.py
+++ graphProgram.py
@@ -4,19 +4,22 @@ from time import sleep, strftime, time
 import matplotlib.pyplot as plt
 
 plt.ion()
-x = [i for i in range(20,100,3)]
+x = [i for i in range(20, 100, 3)]
 y = [i for i in range(len(x))]
 
-#draw a graph with data
+# draw a graph with data
+
+
 def graph():
     plt.clf()
-    plt.scatter(x,y)
-    plt.plot(x,y)
+    plt.scatter(x, y)
+    plt.plot(x, y)
     plt.draw()
+
 
 while True:
     plt.clf()
-    plt.scatter(x,y)
-    plt.plot(x,y)
+    plt.scatter(x, y)
+    plt.plot(x, y)
     plt.draw()
     sleep(1)
