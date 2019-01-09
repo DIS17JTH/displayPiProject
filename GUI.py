044054b5a611174bcedb814210f8a4fcfbbfb5ce
malu1798 <malu1798@student.ju.se>
2019-01-09 15:14:03 +0100

    class added for window

--- GUI.py
+++ GUI.py
@@ -1,13 +1,54 @@
+#!/usr/bin/python3
+
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QWidget, QToolTip,
+                             QPushButton, QMessageBox, QDesktopWidget, QApplication)
+from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QIcon
 
 
-def app():
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.resize(250, 150)
+        self.center()
+        self.setWindowTitle("Weather and time")
+        self.resize(400, 400)
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowIcon(QIcon('train.jpg'))
+        self.button(150, 100)
+        self.show()
+
+    def center(self):
+
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def button(self, x, y):
+        #self.setToolTip('This is a <b>QWidget</b> widget')
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This button does: <b>Nothing</b> :D')
+        btn.resize(btn.sizeHint())
+        btn.move(x-50, y-50)
+
+        qbtn = QPushButton('Quit', self)
+        qbtn.setToolTip('This button: <b>exit</b> the program')
+        qbtn.clicked.connect(QApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(x, y)
+
+
+if __name__ == '__main__':
     my_app = QApplication(sys.argv)
-    w = QWidget()
-    w.setWindowTitle("Weather and time")
-    w.show()
+    window = Window()
+    # window.setToolTip('test')
     sys.exit(my_app.exec_())
 
 
-app()
+#QToolTip.setFont(QFont('SansSerif', 10))
